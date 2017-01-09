d7033238652e3550d92d7328bab87aa3b26c9674
Markus Kokott <markus.kokott@capgemini.com>
2017-01-09 17:10:34 +0100

    added compute resources to webapp module
    Change-Id: I1fb17f15b38b6a612fda53f199e92b5f46f45d71

--- loadbalanced-webapp/main.tf
+++ loadbalanced-webapp/main.tf
@@ -32,3 +32,54 @@ resource "aws_security_group_rule" "egress_rules" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.allow_egress_all_traffic.id}"
 }
+
+# create EC2 instances in auto scaling groups
+## launch confguration of web servers
+resource "aws_launch_configuration" "launch_config_webservers" {
+  image_id                    = "${var.ec2_ami_id}"
+  instance_type               = "${var.ec2_instance_type}"
+  security_groups             = ["${aws_security_group.allow_ingress_http_traffic}"]
+  associate_public_ip_address = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## auto scaling group for web servers
+resource "aws_autoscaling_group" "auto_scaling_group_webservers" {
+  launch_configuration = "${aws_launch_configuration.launch_config_webservers.id}"
+
+  # ! vpc-zone := subnet-id !
+  vpc_zone_identifier = ["${var.subnet_ids}"]
+  min_size            = "${element(var.number_of_instances, "min")}"
+  max_size            = "${element(var.number_of_instances, "max")}"
+  load_balancers      = ["${aws_elb.elb_webservers.id}"]
+  health_check_type   = "ELB"
+}
+
+# elastic load balancer for web servers
+resource "aws_elb" "elb_webservers" {
+  name    = "webservers-elb"
+  subnets = ["${var.subnet_ids}"]
+
+  security_groups = [
+    "${aws_security_group.allow_ingress_http_traffic}",
+    "${aws_security_group.allow_egress_all_traffic}",
+  ]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = 80
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
+  }
+}
--- loadbalanced-webapp/vars.tf
+++ loadbalanced-webapp/vars.tf
@@ -1,13 +1,27 @@
-variable region {
-  description = "id of region to create resources in; default is ap-southeast-2 (Sydney)"
-  type        = "string"
-  default     = "ap-southeast-2"
-}
-
 variable cidr_range_access_to_app {
   description = "list of ip ranges in cidr notation that should be able to access the app. default is the whole internet"
   type        = "list"
   default     = ["0.0.0.0/0"]
+}
+
+variable ec2_ami_id {
+  description = "id of ami to be used for ec2 instances"
+  type        = "string"
+}
+
+variable ec2_instance_type {
+  description = "type of instances to be created in auto scaling group"
+  type        = "string"
+}
+
+variable number_of_instances {
+  description = "mnumber of instances in the auto scaling group. specify minimum number of instances that should be running at all times ('min') and the maximum number of instances the ASG can scale to ('max'). default values are 1 & 3"
+  type        = "map"
+
+  default = {
+    "min" = 1
+    "max" = 3
+  }
 }
 
 variable open_ports_map {
@@ -20,14 +34,10 @@ variable open_ports_map {
   }
 }
 
-variable subnet_ids {
-  description = "list of subnets where ec2 instances should be placed"
-  type = "list"
-}
-
-variable vpc_id {
-  description = "id of vpc in which resources should be created"
-  type = "string"
+variable region {
+  description = "id of region to create resources in; default is ap-southeast-2 (Sydney)"
+  type        = "string"
+  default     = "ap-southeast-2"
 }
 
 variable resource_default_tags {
@@ -37,4 +47,14 @@ variable resource_default_tags {
   default = {
     module = "loadbalanced-webapp"
   }
+}
+
+variable subnet_ids {
+  description = "list of subnets where ec2 instances should be placed"
+  type        = "list"
+}
+
+variable vpc_id {
+  description = "id of vpc in which resources should be created"
+  type        = "string"
 }
